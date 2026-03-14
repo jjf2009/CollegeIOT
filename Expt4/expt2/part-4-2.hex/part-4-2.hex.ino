cef888b95de95e99b95845f3ad4738339af7423c
jff2009 <jaredfurtadowork@gmail.com>
2026-03-14 13:28:05 +0530

    fileadded

---
+++ Expt4/expt2/part-4-2.hex/part-4-2.hex.ino
@@ -0,0 +1,21 @@
+#include<LiquidCrystal.h>
+LiquidCrystal lcd(12,11,5,4,3,2);
+int ledpin = 6;
+int potpin=A0;
+int Value;
+void setup() {
+  pinMode(ledpin,OUTPUT);
+  lcd.begin(16,2);
+}
+
+void loop() {
+  Value = analogRead(potpin);
+  Value = Value / 4;
+  analogWrite(ledpin, Value);
+  lcd.setCursor(0, 0);
+  lcd.print("LED Brightness: ");   // fixed caps too
+  lcd.setCursor(0, 1);
+  lcd.print(Value);
+  lcd.print("   ");                // trailing spaces clear leftover digits
+  delay(100);
+}
