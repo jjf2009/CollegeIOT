f7bfd093e2b8add2a57e495da07765a60cf3c533
jff2009 <jaredfurtadowork@gmail.com>
2026-04-18 15:25:06 +0530

    expt4 comepted

--- Expt4/expt2/part-4-2.hex/part-4-2.hex.ino
+++ Expt4/expt2/part-4-2.hex/part-4-2.hex.ino
@@ -1,22 +1,30 @@
-#include<LiquidCrystal.h>
+#include <LiquidCrystal.h>
 LiquidCrystal lcd(12,11,5,4,3,2);
+
 int ledpin = 6;
-int potpin=A0;
+int potpin = A0;
 float Value;
+
 void setup() {
-  pinMode(ledpin,OUTPUT);
+  pinMode(ledpin, OUTPUT);
   lcd.begin(16,2);
 }
 
 void loop() {
   Value = analogRead(potpin);
-  //Value = Value/402*100;
-  int value = map(Value,0,402,0,100);
-  analogWrite(ledpin, value);
+   int value = map(Value,0,402,0,100);
+
   lcd.setCursor(0, 0);
-  lcd.print("LED Brightness: ");   // fixed caps too
+  lcd.print("LED Brightness:");
+
+  // Clear previous value area
+  lcd.setCursor(0, 1);
+  lcd.print("                ");  // clear full line
+
+  // Print updated value
   lcd.setCursor(0, 1);
   lcd.print(value);
-  lcd.print("%");                // trailing spaces clear leftover digits
+  lcd.print("%");
+
   delay(100);
 }
--- Expt4/expt4/expt-1.hex/expt-1.hex.ino
+++ Expt4/expt4/expt-1.hex/expt-1.hex.ino
@@ -1,5 +1,3 @@
-#include <LiquidCrystal.h>
-LiquidCrystal lcd(12, 11, 5, 4, 3, 2);
 
 int ledPin = 6;
 int analogPin = A0;
@@ -7,36 +5,20 @@ int analogPin = A0;
 int brightness = 0;    
 int fadeAmount = 5;    
 
-int dvalue = 0;
-int fixedValue;
-int fixedPercent;
+
 
 void setup() {
   pinMode(ledPin, OUTPUT);
-  lcd.begin(16, 2);
-  
-  lcd.setCursor(0, 0);
-  lcd.print("Analog Read: ");
+
 }
 
 void loop() {
-  
-  fixedValue = analogRead(analogPin);
-  
-  
-  fixedPercent = map(fixedValue, 0, 1023, 0, 100);
-  dvalue = map(fixedValue,0,1023,0,255);
-  
-  lcd.setCursor(0, 1);
-  
-  
-  analogWrite(ledPin, brightness);
-    lcd.print(brightness);
+  // Update brightness
   brightness = brightness + fadeAmount;
-  
   if (brightness <= 0 || brightness >= 255) {
     fadeAmount = -fadeAmount; 
   }
-  
-  delay(50); 
+  analogWrite(ledPin, brightness);
+
+  delay(50);
 }
